96a2bfa48c8f0f77a1692ab43efd2b8078c24279
hata <hata@example.com>
2024-10-12 08:46:37 +0000

    認証の「formデータを受け取る」所まで書いた

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\TestController;
 // タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
 Route::get('/task/list', [TaskController::class, 'list']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
