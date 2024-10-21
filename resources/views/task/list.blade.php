3416808db53304698dee05cfa98b7941ab90b628
hata <hata@example.com>
2024-10-21 05:31:20 +0000

    タスクの削除と完了を書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -8,8 +8,19 @@
         <h1>タスクの登録</h1>
 
         @if (session('front.task_register_success') == true)
-                タスクを登録しました！！<br>
-            @endif
+            タスクを登録しました！！<br>
+        @endif
+
+        @if (session('front.task_delete_success') == true)
+            タスクを削除しました！！<br>
+        @endif
+
+        @if (session('front.task_completed_success') == true)
+            タスクを完了にしました！！<br>
+        @endif
+        @if (session('front.task_completed_failure') == true)
+            タスクの完了に失敗しました....<br>
+        @endif
 
         @if ($errors->any())
                 <div>
@@ -44,7 +55,11 @@
             <td>{{ $task->getPriorityString() }}
             <td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a>
             <td><a href="{{ route('edit', ['task_id' => $task->id]) }}">編集</a>
-            <td><form action="./top.html"><button>完了</button></form>
+            <td>
+                <form action="{{ route('complete', ['task_id' => $task->id]) }}" method="post">
+                     @csrf
+                     <button onclick='return confirm("このタスクを「完了」にします。よろしいですか？");' >完了</button>
+                </form>
     @endforeach
 
         </table>
